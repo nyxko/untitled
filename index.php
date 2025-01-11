95ddf23ddcd56b46ee306be6935f4087e1102754
Nico <78345044+nicolaepoptoptal@users.noreply.github.com>
2025-01-11 20:04:23 +0200

    Fix HTML tag syntax in index.php to improve language attributes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="<?php language_attributes(); ?>">
+<html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
